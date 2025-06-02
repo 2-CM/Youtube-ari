afa2c3d06bfb7a7d855517d6f91b52bdc63dbdea
로인 <151013952+bboroin@users.noreply.github.com>
2025-06-02 15:26:28 +0900

    feat: UserMenu 구현 #2

--- youtube-ari/src/components/Header/HeaderUserMenu.jsx
+++ youtube-ari/src/components/Header/HeaderUserMenu.jsx
@@ -1,5 +1,17 @@
 const HeaderUserMenu = () => {
-  return <div>사용자메뉴</div>;
+  return (
+    <div className="flex">
+      <button className="flex items-center justify-center border-none px-[6px] py-[1px] outline-none">
+        <div className="mx-2 h-8 w-8 cursor-pointer overflow-hidden rounded-full">
+          <img
+            src=""
+            alt="Avatar Image"
+            className="block h-8 w-8 bg-slate-700"
+          />
+        </div>
+      </button>
+    </div>
+  );
 };
 
 export default HeaderUserMenu;
