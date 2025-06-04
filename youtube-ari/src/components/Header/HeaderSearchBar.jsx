65a1001f7a18897308c561302954900ef76e69fc
로인 <151013952+bboroin@users.noreply.github.com>
2025-06-04 17:59:50 +0900

    style: 사용된 아이콘을 lucide icons로 변경 #2

--- youtube-ari/src/components/Header/HeaderSearchBar.jsx
+++ youtube-ari/src/components/Header/HeaderSearchBar.jsx
@@ -1,5 +1,4 @@
-import searchIcon from "../../assets/search-icon.svg";
-import voiceSearchIcon from "../../assets/voice-search-icon.svg";
+import { Search, Mic } from "lucide-react";
 
 const HeaderSearchBar = () => {
   return (
@@ -18,32 +17,28 @@ const HeaderSearchBar = () => {
               aria-controls="autocomplete-list"
               className="peer my-0 w-full appearance-none border-none bg-transparent py-px text-base font-normal leading-[22px] antialiased shadow-none outline-none"
             />
-            <img
-              src={searchIcon}
+            <Search
+              strokeWidth={1}
               alt="Search Icon"
               className="pointer-events-none absolute left-4 top-1/2 hidden h-5 w-5 translate-y-[-50%] peer-focus:block"
             />
           </form>
         </div>
         <button
-          aria-label="Search"
+          aria-label="Search Btn"
           title="Search"
           className="m-0 w-16 cursor-pointer justify-center rounded-r-[40px] border border-[#d3d3d3] bg-[#f8f8f8] p-0 text-inherit hover:border-[#c6c6c6] hover:bg-[#f0f0f0]"
         >
-          <img
-            src={searchIcon}
+          <Search
             alt="Search Btn"
             className="inline-flex h-6 w-6"
+            strokeWidth={1}
           />
         </button>
       </div>
       <div className="group relative ml-3 rounded-full bg-[#f2f2f2] hover:bg-[#d9d9d9]">
         <button className="inline-flex h-10 w-10 items-center justify-center rounded-full">
-          <img
-            src={voiceSearchIcon}
-            alt="Voice Search Btn"
-            className="h-6 w-6"
-          />
+          <Mic strokeWidth={1.25} alt="Voice Search Btn" className="h-6 w-6" />
         </button>
         <div
           role="tooltip"
