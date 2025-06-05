b14a7fd364b4d35a2e0b717493eb44df9fba9491
로인 <151013952+bboroin@users.noreply.github.com>
2025-06-05 17:26:02 +0900

    refactor: 내부 링크 a 태그를 react-router-dom의 Link로 변경 #5

--- youtube-ari/src/components/SideBar.jsx
+++ youtube-ari/src/components/SideBar.jsx
@@ -5,8 +5,8 @@ const SideBar = () => {
     <div className="fixed bottom-0 left-0 top-14 z-[2028] box-border inline-block w-[72px] px-1">
       <div className="mt-1 flex flex-col">
         <div className="relative inline-block rounded-lg bg-transparent hover:bg-[#f2f2f2] active:bg-[#d9d9d9]">
-          <a
-            href="/"
+          <Link
+            to="/"
             title="Home"
             className="flex w-16 flex-col items-center justify-center py-4 outline-none"
           >
@@ -16,11 +16,11 @@ const SideBar = () => {
             <span className="block max-w-full overflow-ellipsis whitespace-nowrap text-[10px] font-normal">
               Home
             </span>
-          </a>
+          </Link>
         </div>
         <div className="relative inline-block rounded-lg bg-transparent hover:bg-[#f2f2f2] active:bg-[#d9d9d9]">
-          <a
-            href="/"
+          <Link
+            to="/"
             title="You"
             className="flex w-16 flex-col items-center justify-center pb-[14px] pt-4 outline-none"
           >
@@ -34,7 +34,7 @@ const SideBar = () => {
             <span className="block max-w-full overflow-ellipsis whitespace-nowrap text-[10px] font-normal">
               You
             </span>
-          </a>
+          </Link>
         </div>
       </div>
     </div>
