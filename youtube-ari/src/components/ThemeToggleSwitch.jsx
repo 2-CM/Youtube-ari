c3e029fce6197bb119c6b3aaf889c515f26b0089
로인 <151013952+bboroin@users.noreply.github.com>
2025-06-09 19:54:37 +0900

    feat: ThemeToggleSwitch에 title 속성 추가 #2

--- youtube-ari/src/components/ThemeToggleSwitch.jsx
+++ youtube-ari/src/components/ThemeToggleSwitch.jsx
@@ -4,14 +4,18 @@ import { Sun, Moon } from "lucide-react";
 export default function ThemeToggleSwitch() {
   const [theme, setTheme] = useState("light");
 
-  const toggleTheme = () => {
+  const themeToggle = () => {
     setTheme((prev) => (prev === "light" ? "dark" : "light"));
   };
+
+  const themeToggleTitle =
+    theme === "dark" ? "Switch to light theme" : "Switch to dark theme";
 
   return (
     <div className="mr-2 px-2">
       <button
-        onClick={toggleTheme}
+        onClick={themeToggle}
+        title={themeToggleTitle}
         className={`flex h-8 w-14 items-center rounded-full p-1 transition-colors duration-300 ${theme === "dark" ? "bg-gray-700 outline outline-1 outline-white" : "bg-gray-200"}`}
       >
         <div
