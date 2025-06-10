bddcf870379fff1ba19c394c2e21f9c2cfc4e66d
로인 <151013952+bboroin@users.noreply.github.com>
2025-06-10 14:45:50 +0900

    style: 재사용 가능한 컴포넌트 스타일 정의 #5

--- youtube-ari/src/components/SideBar.jsx
+++ youtube-ari/src/components/SideBar.jsx
@@ -5,26 +5,16 @@ const SideBar = () => {
   return (
     <div className="fixed bottom-0 left-0 top-14 z-[2028] box-border inline-block w-[72px] px-1">
       <div className="mt-1 flex flex-col">
-        <div className="relative inline-block rounded-lg bg-transparent hover:bg-[#f2f2f2] active:bg-[#d9d9d9]">
-          <Link
-            to="/"
-            title="Home"
-            className="flex w-16 flex-col items-center justify-center py-4 outline-none"
-          >
-            <div className="mb-[6px]">
+        <div className="sidebarIconWrapper">
+          <Link to="/" title="Home" className="sidebarIconLink">
+            <div className="mb-2">
               <House alt="Home Page" strokeWidth={1.5} className="h-6 w-6" />
             </div>
-            <span className="block max-w-full overflow-ellipsis whitespace-nowrap text-[10px] font-normal">
-              Home
-            </span>
+            <span className="sidebarIconText">Home</span>
           </Link>
         </div>
-        <div className="relative inline-block rounded-lg bg-transparent hover:bg-[#f2f2f2] active:bg-[#d9d9d9]">
-          <Link
-            to="/"
-            title="You"
-            className="flex w-16 flex-col items-center justify-center pb-[14px] pt-4 outline-none"
-          >
+        <div className="sidebarIconWrapper">
+          <Link to="/" title="You" className="sidebarIconLink">
             <div className="mb-2">
               <UserCircle
                 alt="User Page"
@@ -32,9 +22,7 @@ const SideBar = () => {
                 className="h-6 w-6"
               />
             </div>
-            <span className="block max-w-full overflow-ellipsis whitespace-nowrap text-[10px] font-normal">
-              You
-            </span>
+            <span className="sidebarIconText">You</span>
           </Link>
         </div>
       </div>
