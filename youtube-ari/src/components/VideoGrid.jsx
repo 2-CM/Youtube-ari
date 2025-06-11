4f7c79b4fd1680f27bc73bd90a55c3dce3d60018
로인 <151013952+bboroin@users.noreply.github.com>
2025-06-11 17:54:58 +0900

    feat: VideoGrid 에 mockVideos 기반으로 VideoCard 리스트 렌더링 #7

--- youtube-ari/src/components/VideoGrid.jsx
+++ youtube-ari/src/components/VideoGrid.jsx
@@ -1,22 +1,22 @@
 import VideoCard from "./VideoCard";
+import mockVideos from "../data/mockVideos";
 
 const VideoGrid = () => {
   return (
     <div className="ml-[72px] mt-14 bg-gray-100">
       <div className="mx-4 flex justify-center">
         <div className="grid w-full grid-cols-3 justify-start pt-6">
-          <VideoCard />
-          <VideoCard />
-          <VideoCard />
-          <VideoCard />
-          <VideoCard />
-          <VideoCard />
-          <VideoCard />
-          <VideoCard />
-          <VideoCard />
-          <VideoCard />
-          <VideoCard />
-          <VideoCard />
+          {mockVideos.map((video) => (
+            <VideoCard
+              key={video.id}
+              title={video.title}
+              thumbnail={video.thumbnail}
+              channelImage={video.channelImage}
+              channelName={video.channelName}
+              views={video.views}
+              publishedAt={video.publishedAt}
+            />
+          ))}
         </div>
       </div>
     </div>
