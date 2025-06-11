dfca6ded653c3c44852b1a1413ad812d35c0f624
로인 <151013952+bboroin@users.noreply.github.com>
2025-06-11 18:19:28 +0900

    feat: 반응형 그리드로 VideoCard 개수 조절 (sm:1, md:2, lg:3) #7

--- youtube-ari/src/components/VideoGrid.jsx
+++ youtube-ari/src/components/VideoGrid.jsx
@@ -3,20 +3,22 @@ import mockVideos from "../data/mockVideos";
 
 const VideoGrid = () => {
   return (
-    <div className="ml-[72px] mt-14 bg-gray-100">
+    <div className="ml-[72px] mt-14">
       <div className="mx-4 flex justify-center">
-        <div className="grid w-full grid-cols-3 justify-start pt-6">
-          {mockVideos.map((video) => (
-            <VideoCard
-              key={video.id}
-              title={video.title}
-              thumbnail={video.thumbnail}
-              channelImage={video.channelImage}
-              channelName={video.channelName}
-              views={video.views}
-              publishedAt={video.publishedAt}
-            />
-          ))}
+        <div className="grid w-full grid-cols-1 justify-start pt-6 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+          {[...Array(4)]
+            .flatMap(() => mockVideos)
+            .map((video, index) => (
+              <VideoCard
+                key={index}
+                title={video.title}
+                thumbnail={video.thumbnail}
+                channelImage={video.channelImage}
+                channelName={video.channelName}
+                views={video.views}
+                publishedAt={video.publishedAt}
+              />
+            ))}
         </div>
       </div>
     </div>
