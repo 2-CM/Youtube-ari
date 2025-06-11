b66d1afc3c0f8d847bb33af96c129ace66a7f5dd
로인 <151013952+bboroin@users.noreply.github.com>
2025-06-11 16:56:59 +0900

    feat: VideoCard 초기 구현 #7

---
+++ youtube-ari/src/components/VideoCard.jsx
@@ -0,0 +1,73 @@
+import { EllipsisVertical } from "lucide-react";
+import thumnail from "../assets/mock_thumnail.png";
+import channelImage from "../assets/mock_channelImage.jpg";
+
+const VideoCard = () => {
+  return (
+    <div className="relative mx-2 mb-8">
+      <div className="flex h-full items-start justify-center">
+        <div className="relative block w-full min-w-[327px] max-w-[700px]">
+          <div className="relative flex h-full flex-col">
+            <div className="relative block w-full">
+              <a href="/" className="block overflow-hidden rounded-xl">
+                <img
+                  src={thumnail}
+                  alt=""
+                  className="inline-block h-full w-full object-cover"
+                />
+              </a>
+            </div>
+            <div className="relative flex flex-auto cursor-pointer flex-row">
+              <div className="mr-3 mt-3 inline-block h-9">
+                <a href="/">
+                  <div className="relative flex h-9 w-9 justify-center">
+                    <img
+                      src={channelImage}
+                      alt=""
+                      className="rounded-full object-fill"
+                    />
+                  </div>
+                </a>
+              </div>
+              <div className="overflow-x-hidden pr-6">
+                <h3 className="mb-1 mt-3">
+                  <a href="/" className="block">
+                    <div className="line-clamp-2 max-h-11 text-ellipsis whitespace-normal">
+                      𝒑𝒍𝒂𝒚𝒍𝒊𝒔𝒕 이게 진짜 노동요지! 시원하게 그루브 한 잔해🍸😎 |
+                      쌈뽕한 그루브 & 베이스라인 팝송 플리
+                    </div>
+                  </a>
+                </h3>
+                <div className="flex flex-col text-left text-sm text-ytGray-90">
+                  <div className="flex flex-wrap items-center">
+                    <a href="/" className="hover:text-black">
+                      모카냥 MoccaCat
+                    </a>
+                  </div>
+                  <div className="line-clamp-2 flex flex-row text-ellipsis">
+                    <span>29K views </span>
+                    <span className="ml-1">• 13 days ago</span>
+                  </div>
+                </div>
+              </div>
+              <div className="absolute -right-3 top-1">
+                <button className="relative inline-block h-10 w-10 align-middle">
+                  <div className="flex items-center justify-center">
+                    <EllipsisVertical
+                      strokeWidth={1}
+                      fill="black"
+                      alt="videoCard Menu"
+                      className="h-6 w-6"
+                    />
+                  </div>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default VideoCard;
