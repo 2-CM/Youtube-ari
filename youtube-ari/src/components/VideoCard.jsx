78c04c05425bb17d1d653d2f33e41e222fc14e21
로인 <151013952+bboroin@users.noreply.github.com>
2025-06-11 23:54:45 +0900

    feat: 채널명 및 영상 제목에 hover 시 title 속성과 tooltip 제공 #7

--- youtube-ari/src/components/VideoCard.jsx
+++ youtube-ari/src/components/VideoCard.jsx
@@ -23,29 +23,37 @@ const VideoCard = ({
           </div>
           <div className="relative flex flex-row">
             <div className="mr-3 mt-3 h-9">
-              <a href="/">
-                <div className="flex h-9 w-9 justify-center">
-                  <img
-                    src={channelImage}
-                    alt="Channel Image"
-                    className="rounded-full object-cover"
-                  />
-                </div>
+              <a href="/" className="flex h-9 w-9 justify-center">
+                <img
+                  src={channelImage}
+                  alt="Channel Image"
+                  className="rounded-full object-cover"
+                />
               </a>
             </div>
             <div className="overflow-x-hidden pr-6">
               <h3 className="mb-1 mt-3">
                 <a href="/" className="block">
-                  <div className="line-clamp-2 max-h-11 text-ellipsis whitespace-normal">
+                  <div
+                    title={title}
+                    className="line-clamp-2 max-h-11 text-ellipsis whitespace-normal"
+                  >
                     {title}
                   </div>
                 </a>
               </h3>
               <div className="flex flex-col text-left text-sm text-ytGray-90">
-                <div className="flex flex-wrap items-center">
-                  <a href="/" className="hover:text-black">
+                <div className="group flex flex-wrap items-center">
+                  <a href="/" title={channelName} className="hover:text-black">
                     {channelName}
                   </a>
+                  <div
+                    role="tooltip"
+                    aria-label="tooltip"
+                    className="tooltip -top-1 left-8 group-hover:opacity-90"
+                  >
+                    {channelName}
+                  </div>
                 </div>
                 <div className="line-clamp-2 flex flex-row text-ellipsis">
                   <span>{views}</span>
