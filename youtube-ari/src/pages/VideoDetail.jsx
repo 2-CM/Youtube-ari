7f991565132745095e4ea9525a38da384738cf64
로인 <151013952+bboroin@users.noreply.github.com>
2025-06-23 15:39:29 +0900

    feat: VideoDetail에서 URL 파라미터 기반으로 video 데이터 매칭 및 제목 출력 구현 #9

--- youtube-ari/src/pages/VideoDetail.jsx
+++ youtube-ari/src/pages/VideoDetail.jsx
@@ -1,8 +1,26 @@
+import { useParams } from "react-router-dom";
+
+import mockVideos from "../data/mockVideos";
+
 import ChannelInfo from "../components/VideoDetail/ChannelInfo";
 import Description from "../components/VideoDetail/Description";
 import RelatedVideos from "../components/VideoDetail/RelatedVideos";
 
 const VideoDetail = () => {
+  // VideoDetail에서 URL param을 통해 mockVideos에서 해당 video 찾기
+  const { id } = useParams();
+  const video = mockVideos.find((v) => v.videoId === Number(id));
+
+  const {
+    title,
+    thumbnail,
+    channelName,
+    channelImage,
+    views,
+    publishedAt,
+    description,
+  } = video;
+
   return (
     <div className="mx-auto mt-14 flex flex-row">
       <div className="ml-6 flex-1 pr-6 pt-6">
@@ -17,8 +35,11 @@ const VideoDetail = () => {
             allowfullscreen
           ></iframe>
         </div>
-        <ChannelInfo />
-        <Description />
+        <div className="mb-6 mt-3">
+          <h1>{title}</h1>
+          <ChannelInfo />
+          <Description />
+        </div>
       </div>
       <div className="pr-6 pt-6">
         <RelatedVideos />
