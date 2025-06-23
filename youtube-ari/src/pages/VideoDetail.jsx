d442968ea7c18ecba7675ceb1a6ffcb70a502dca
로인 <151013952+bboroin@users.noreply.github.com>
2025-06-23 15:11:23 +0900

    feat: iframe 태그를 사용한 영상 삽입 및 레이아웃 구현 #9

--- youtube-ari/src/pages/VideoDetail.jsx
+++ youtube-ari/src/pages/VideoDetail.jsx
@@ -1,18 +1,28 @@
 import ChannelInfo from "../components/VideoDetail/ChannelInfo";
 import Description from "../components/VideoDetail/Description";
 import RelatedVideos from "../components/VideoDetail/RelatedVideos";
-import { useParams } from "react-router-dom";
 
 const VideoDetail = () => {
-  const { id } = useParams();
-
   return (
-    <div className="mt-14">
-      <h1>Video Detail Page - Video ID: {id}</h1>
-      <div>Video</div>
-      <ChannelInfo />
-      <Description />
-      <RelatedVideos />
+    <div className="mx-auto mt-14 flex flex-row">
+      <div className="ml-6 flex-1 pr-6 pt-6">
+        <div className="aspect-video overflow-hidden rounded-lg">
+          <iframe
+            className="h-full w-full"
+            src="https://www.youtube.com/embed/C-hQpbS5gHI?si=aPtc1QWE5sgLTjda"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerpolicy="strict-origin-when-cross-origin"
+            allowfullscreen
+          ></iframe>
+        </div>
+        <ChannelInfo />
+        <Description />
+      </div>
+      <div className="pr-6 pt-6">
+        <RelatedVideos />
+      </div>
     </div>
   );
 };
