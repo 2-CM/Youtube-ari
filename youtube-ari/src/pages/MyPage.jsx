a9decf7d5788622330ceb9e2a62eab93983fbda5
로인 <151013952+bboroin@users.noreply.github.com>
2025-07-22 18:55:41 +0900

    feat: MyPage 로그인 필요 안내 UI 구현 #17

--- youtube-ari/src/pages/MyPage.jsx
+++ youtube-ari/src/pages/MyPage.jsx
@@ -1,10 +1,35 @@
 import VideoCard from "../components/VideoGrid/VideoCard";
 import mockVideos from "../data/mockVideos";
 
-import { ChevronRight, ChevronLeft, LogOut } from "lucide-react";
+import {
+  ChevronRight,
+  ChevronLeft,
+  LogOut,
+  UserCircle,
+  TvMinimalPlay,
+} from "lucide-react";
 
-const MyPage = ({ user, logout }) => {
+const MyPage = ({ user, login, logout }) => {
   const historyVideos = mockVideos.slice(0, 4);
+
+  if (!user) {
+    return (
+      <div className="ml-[72px] flex flex-col items-center justify-center px-6 pt-32">
+        <TvMinimalPlay strokeWidth={1} className="h-32 w-32" />
+        <p className="mb-4 mt-10 text-center text-xl font-semibold">
+          이 페이지는 로그인 후 이용할 수 있습니다.
+        </p>
+        <p className="text-md pb-10 text-ytGray-90 dark:text-ytGray-20">
+          나만의 시청 기록을 확인하려면 로그인하세요.
+        </p>
+
+        <button onClick={login} className="signInBtn">
+          <UserCircle strokeWidth={1.25} className="-ml-2 mr-2 h-6 w-6" />
+          <span className="font-medium">Sign in</span>
+        </button>
+      </div>
+    );
+  }
 
   return (
     <>
