71783dd2123eb8526051d71b42de82b90e608ad2
로인 <151013952+bboroin@users.noreply.github.com>
2025-08-20 14:31:55 +0900

    feat: ScrollToTop 컴포넌트 추가 및 App에 적용 #25

--- youtube-ari/src/App.jsx
+++ youtube-ari/src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 
 import Header from "./components/Header/Header";
+import ScrollToTop from "./components/common/ScrollToTop";
 import Home from "./pages/Home";
 import MyPage from "./pages/MyPage";
 import VideoDetail from "./pages/VideoDetail";
@@ -13,6 +14,7 @@ function App() {
   return (
     <div className="min-h-screen bg-white text-black dark:bg-darkBg dark:text-white">
       <Header />
+      <ScrollToTop />
       <Routes>
         <Route element={<LayoutWithSidebar />}>
           <Route path="/" element={<Home />} />
