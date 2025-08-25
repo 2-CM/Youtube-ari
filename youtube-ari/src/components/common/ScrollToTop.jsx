47c571e9f50f5b607bb85a779ee2902d0760aa11
로인 <151013952+bboroin@users.noreply.github.com>
2025-08-25 18:41:29 +0900

    fix: 검색어 변경 시 스크롤이 최상단으로 이동하지 않는 문제 수정 #27

--- youtube-ari/src/components/common/ScrollToTop.jsx
+++ youtube-ari/src/components/common/ScrollToTop.jsx
@@ -2,11 +2,11 @@ import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
 const ScrollToTop = () => {
-  const { pathname } = useLocation();
+  const { pathname, search } = useLocation();
 
   useEffect(() => {
-    window.scrollTo(0, 0); // 페이지 이동 시 스크롤 최상단으로 이동
-  }, [pathname]);
+    window.scrollTo(0, 0); // 페이지 이동 또는 쿼리 변경 시 스크롤 최상단으로 이동
+  }, [pathname, search]);
 
   return null;
 };
